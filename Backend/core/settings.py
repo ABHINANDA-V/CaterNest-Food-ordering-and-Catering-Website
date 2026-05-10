fd167ccbd22248dc93e8aa669f0ce881fbacb566
ABHINANDA-V <abhinandav96@gmail.com>
2026-05-10 21:13:07 +0530

    Debug cloudinary

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -173,3 +173,15 @@ cloudinary.config(
 )
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
+    'API_KEY': os.getenv("CLOUDINARY_API_KEY"),
+    'API_SECRET': os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+print("Cloud Name:", os.getenv("CLOUDINARY_CLOUD_NAME"))
+print("API Key:", os.getenv("CLOUDINARY_API_KEY"))
+print("Storage:", DEFAULT_FILE_STORAGE)
